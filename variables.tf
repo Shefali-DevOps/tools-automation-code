64f4a26d46c5dc80c669fe9eae7baabd1e9303ba
Shefali <shefalimurarka19@gmail.com>
2025-04-06 16:19:39 +0530

    tools- launch an minikube instance

--- variables.tf
+++ variables.tf
@@ -2,12 +2,14 @@ variable "tools"{
   default = {
 
     github-runner = {
+      name    = "github-runner"
       instance_type = "t2.micro"
       policy_name = ["AdministratorAccess"]
       ports = {}
     }
 
     vault= {
+      name = "vault"
       instance_type = "t2.micro"
       policy_name = []
       ports= {
@@ -15,8 +17,18 @@ variable "tools"{
       }
     }
 
+    minikube= {
+      name = "minikube"
+      instance_type = "t3.medium"
+      policy_actions = []
+      volume_size  = 30
+      ports= {
+        kube = 8443
+      }
+    }
+
+    }
   }
-}
 
 variable "hosted_zone_id" {
   default = "Z04468998YJS3W1N2Q1P"
