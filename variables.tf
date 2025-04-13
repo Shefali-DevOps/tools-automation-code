68d7258461dbcbd566a6babed2b4d7cf605ae37c
Shefali <shefalimurarka19@gmail.com>
2025-04-13 13:52:51 +0530

    tools- Commented minikube

--- variables.tf
+++ variables.tf
@@ -17,16 +17,16 @@ variable "tools"{
       volume_size = 20
     }
 
-    minikube= {
-      instance_type = "t3.medium"
-      policy_name = []
-      ports= {
-        kube = 8443
+    # minikube= {
+    #   instance_type = "t3.medium"
+    #   policy_name = []
+    #   ports= {
+    #     kube = 8443
+    #   }
+    #   volume_size = 30
+    # }
+    #
       }
-      volume_size = 30
-    }
-
-    }
   }
 
 variable "hosted_zone_id" {
